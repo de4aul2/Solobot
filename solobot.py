82d75fc42938af917e4a3512bf1652a829353e7d
de4aul2 <alex.kostet@yandex.ru>
2025-11-27 19:41:19 +0400

    Функция обновления номера телефона

--- solobot.py
+++ solobot.py
@@ -11,6 +11,19 @@ def message_handler(message):
     bot.send_message(message.chat.id, get_cfg()['Ключ API CRM системы'])
 
 
+@bot.callback_query_handler(func=lambda callback: True)
+def callback_message(callback):
+    data_parts = callback.data.split('_')
+    action = data_parts[0]
+    userid = data_parts[1] if len(data_parts) > 1 else None
+    if action == 'Пропустить':
+        print(f'Пропустить пользователя: {userid}')
+        bot.delete_message(callback.message.chat.id, callback.message.message_id)
+    elif action == 'Заменить номер':
+        print(f'Заменить номер для пользователя: {userid}')
+        bot.delete_message(callback.message.chat.id, callback.message.message_id)
+        replace_number(userid)
+
 
 bot.delete_webhook()
 bot.polling(none_stop=True, timeout=60)
